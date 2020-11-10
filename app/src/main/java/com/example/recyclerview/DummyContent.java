c474039dbde4628532408f6b15e2d09b4bda5249
nadiabenko <nadiabenkoo@gmail.com>
2020-11-10 18:21:11 +0700

    Add DummyContent

--- app/src/main/java/com/example/recyclerview/DummyContent.java
+++ app/src/main/java/com/example/recyclerview/DummyContent.java
@@ -18,3 +18,20 @@ public class DummyContent {
         }
     }
 
+    /** Создание класса элементов для списка. */
+    public static class DummyItem {
+        public final String id;
+        public final String content;
+        public final String details;
+
+        public DummyItem(String id, String content, String details) {
+            this.id = id;
+            this.content = content;
+            this.details = details;
+        }
+        @Override
+        public String toString() { return content; }
+    }
+}
+
+
